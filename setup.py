e61b104707c841e17e6aa909793c5c85dc7762fb
ludwig.schwardt <ludwig.schwardt@afb03321-2ffc-5eb7-50a2-9de243b5230f>
2011-05-27 15:25:12 +0000

    Added README and INSTALL files, based on project description and Installation wiki page, respectively. These RST files are converted by wikir to the Google Code wiki format, by running e.g. "wikir trunk/INSTALL.rst > wiki/Installation.wiki" in the top-level project source directory. Updated setup.py in anticipation of uploading mplh5canvas to PyPI.

--- setup.py
+++ setup.py
@@ -2,16 +2,39 @@
 from setuptools import setup, find_packages
 from matplotlib import __version__
 import sys
+import os.path
 
 if tuple([int(x) for x in __version__.split(".")[:3]]) < (0, 99, 1):
-   print "The HTML 5 Canvas Backend requires matplotlib 0.99.1.1 or newer. Your version (%s) appears older than this. Unable to continue..." % __version__
-   sys.exit(0)
+    print "The HTML5 Canvas Backend requires matplotlib 0.99.1.1 or newer. Your version (%s) appears older than this. Unable to continue..." % __version__
+    sys.exit(0)
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+INSTALL = open(os.path.join(here, 'INSTALL.rst')).read()
+
+VERSION = "trunk"
+DESCRIPTION = "A matplotlib backend based on HTML5 Canvas."
+LONG_DESCRIPTION = README + "\n\n" + INSTALL
+CLASSIFIERS = [
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "Intended Audience :: End Users/Desktop",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 2",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
 
 setup (
-    name = "mplh5canvas",
-    version = "trunk",
-    description = "Matplotlib HTML 5 Canvas Backend",
-    author = "Simon Ratcliffe, Ludwig Schwardt",
+    name="mplh5canvas",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=CLASSIFIERS,
+    author="Simon Ratcliffe, Ludwig Schwardt",
+    author_email="sratcliffe@ska.ac.za, ludwig.schwardt@gmail.com",
+    url="http://code.google.com/p/mplh5canvas/",
+    license="BSD",
     packages = find_packages(),
     scripts = [],
     zip_safe = False,
